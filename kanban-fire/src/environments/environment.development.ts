de38a98b9c4e59958848c68697558e1457b790ce
FelipeSilvaTabo <112593208+FelipeSilvaTabo@users.noreply.github.com>
2023-04-24 09:28:37 -0300

    Add files via upload

---
+++ kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-622e5',
+    appId: '1:887077749560:web:73090952070c8fffdff7cd',
+    storageBucket: 'kanbanfire-622e5.appspot.com',
+    apiKey: 'AIzaSyDMml7jIdLeVj2gC5x-_0GS80ly7pA2k0E',
+    authDomain: 'kanbanfire-622e5.firebaseapp.com',
+    messagingSenderId: '887077749560',
+    measurementId: 'G-LGJHXED66H',
+  },};
---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-622e5',
+    appId: '1:887077749560:web:73090952070c8fffdff7cd',
+    storageBucket: 'kanbanfire-622e5.appspot.com',
+    apiKey: 'AIzaSyDMml7jIdLeVj2gC5x-_0GS80ly7pA2k0E',
+    authDomain: 'kanbanfire-622e5.firebaseapp.com',
+    messagingSenderId: '887077749560',
+    measurementId: 'G-LGJHXED66H',
+  },};
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDMml7jIdLeVj2gC5x-_0GS80ly7pA2k0E",
+  authDomain: "kanbanfire-622e5.firebaseapp.com",
+  projectId: "kanbanfire-622e5",
+  storageBucket: "kanbanfire-622e5.appspot.com",
+  messagingSenderId: "887077749560",
+  appId: "1:887077749560:web:73090952070c8fffdff7cd",
+  measurementId: "G-LGJHXED66H"
+};
